250983630382573233dfdbdefc49e4aa9f801c1a
hmmChase <hmmchase@gmail.com>
2018-05-17 12:41:37 -0600

    Make Header a controlled form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { Header } from '../Header/Header';
+import Header from '../Header/Header';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import './App.css';
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import { NavLink, Redirect } from 'react-router-dom';
+import { fetchUserData } from '../../utils/fetchUserData';
 
-export const Header = () => {
+class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
   // const displayLoggedIn = (
   //   <div>
   //     <p>Welcome userName</p>
@@ -21,12 +30,55 @@ export const Header = () => {
   //   </div>
   // );
 
-  return (
-    <header>
-      <NavLink to="/">
-        <h1>MOVIE TRACKER</h1>
-      </NavLink>
-    </header>
-  );
-};
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const userData = await fetchUserData();
+    const findUser = userData.data.find(
+      user => user.email === this.state.email
+    );
+    findUser.password === this.state.password 
+      ? <Redirect to="/" />
+      : alert('Incorrect Password');
+    this.setState({
+      email: '',
+      password: ''
+    });
+  };
+
+  render() {
+    return (
+      <header>
+        <NavLink to="/">
+          <h1>MOVIE TRACKER</h1>
+        </NavLink>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <button>Login</button>
+        </form>
+      </header>
+    );
+  }
+}
 // { userLoggedIn ? displayLoggedIn : displayLoggedOut }
+
+export default Header;
