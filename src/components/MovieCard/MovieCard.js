d14d3e217bcfeebef42461a328814cece8485f1b
hmmChase <hmmchase@gmail.com>
2018-05-18 18:55:19 -0600

    Truncate movie overview

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './MovieCard.css';
 
+<<<<<<< HEAD
 const MovieCard = ({ movieId, posterPath, title, releaseDate, voteAverage, overview, toggleFavorite }) => {
   const baseUrl = 'https://image.tmdb.org/t/p/w500';
+=======
+export const MovieCard = props => {
+  const {
+    movieId,
+    posterPath,
+    title,
+    releaseDate,
+    voteAverage,
+    overview
+  } = props;
+  const baseUrl = 'https://image.tmdb.org/t/p/w200';
+>>>>>>> Truncate movie overview
   const imageUrl = baseUrl + posterPath;
 
   return (
     <div className="MovieCard" id={movieId}>
       <img src={imageUrl} alt="movie poster" />
+<<<<<<< HEAD
       <h2>{title}</h2>
       <p>Release Date: {releaseDate}</p>
       <p>Vote Average: {voteAverage}</p>
@@ -18,3 +32,18 @@ const MovieCard = ({ movieId, posterPath, title, releaseDate, voteAverage, overv
 };
 
 export default MovieCard;
+=======
+      <div clasName="card-body">
+        <h2>{title}</h2>
+        <p>Release Date: {releaseDate}</p>
+        <p>Vote Average: {voteAverage}</p>
+        <p>
+          {overview.length > 100
+            ? overview.substring(0, 100) + '...'
+            : overview}
+        </p>
+      </div>
+    </div>
+  );
+};
+>>>>>>> Truncate movie overview
