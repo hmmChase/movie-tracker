72a66aae458584a91ce0d103084a2ec2fc0afe42
hmmChase <hmmchase@gmail.com>
2018-05-20 12:57:08 -0600

    Refactor line lengths

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,27 +3,29 @@ import './MovieCard.css';
 
 const MovieCard = props => {
   const {
-    movieId,
-    posterPath,
+    movie_id,
+    poster_path,
     title,
-    releaseDate,
-    voteAverage,
+    release_date,
+    vote_average,
     overview,
     toggleFavorite
   } = props;
   const baseUrl = 'https://image.tmdb.org/t/p/w200';
-  const imageUrl = baseUrl + posterPath;
+  const imageUrl = baseUrl + poster_path;
 
   return (
-    <div className="MovieCard" id={movieId}>
+    <div className="MovieCard" id={movie_id}>
       <img src={imageUrl} alt="movie poster" />
-      <button onClick={() => toggleFavorite(movieId)}>
-        Add to Favorites
-      </button>
+      <button onClick={() => toggleFavorite(movie_id)}>Add to Favorites</button>
       <div className="card-body">
         <h2>{title}</h2>
-        <p><span className="bold-text">Release Date:</span> {releaseDate}</p>
-        <p><span className="bold-text">Vote Average:</span> {voteAverage}</p>
+        <p>
+          <span className="bold-text">Release Date:</span> {release_date}
+        </p>
+        <p>
+          <span className="bold-text">Vote Average:</span> {vote_average}
+        </p>
         <p>
           {overview.length > 100
             ? overview.substring(0, 100) + '...'
