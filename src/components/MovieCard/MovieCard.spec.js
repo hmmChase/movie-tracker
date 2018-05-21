c43feeea6632fb51db71c4821a549dda71ecc3e9
Ecksi <jimmy.dechant@gmail.com>
2018-05-20 19:25:26 -0600

    Add tests for MovieCard

---
+++ src/components/MovieCard/MovieCard.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MovieCard from './MovieCard';
+
+describe('MovieCard', () => {
+  let movieCard;
+  let mockProps;
+
+  beforeEach(() => {
+    mockProps = {
+      movie_id: 12,
+      poster_path: 'some_path',
+      title: 'blitz',
+      release_date: '12-13-14',
+      vote_average: 8,
+      overview: 'hi ho',
+      toggleFavorite: jest.fn(),
+      isFavorite: jest.fn()
+    };
+    movieCard = shallow(<MovieCard {...mockProps} />);
+  });
+
+  it('matches the snapshot', () => {
+    expect(movieCard).toMatchSnapshot();
+  });
+});
